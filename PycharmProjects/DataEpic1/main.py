979f5c12288b54a48106e74b92d00bb67d81a483
Feyi Bosslady <mac@Feyis-MacBook-Air.local>
2024-06-26 20:31:18 +0100

    a commit

---
+++ PycharmProjects/DataEpic1/main.py
@@ -0,0 +1,15 @@
+a = 7
+b = 24
+main1 = "Feyisayo"
+main2 = "My Love"
+main3 = "Family"
+main4 = "Baby"
+main5 = "Friendship"
+main6 = "Life needs to be easy"
+bool = True
+bool2 = False
+import random
+b=(random.randrange(1,20))
+print(b)
+
+
